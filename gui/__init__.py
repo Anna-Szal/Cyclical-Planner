9c72ba712145406d161f818ea925f4e4661f8bc5
Anna Szal <anna.w.szal@gmail.com>
2024-05-21 09:15:15 +0200

    refact

--- gui/__init__.py
+++ gui/__init__.py
@@ -4,8 +4,8 @@ from kivy.lang import Builder
 # imports needed in .kv files:
 from .page_planner.planner_layout import PlannerLayout
 from .page_planner.task_list import TaskList
-from .page_planner.calendar_widget.calendar_widget import CalendarWidget
-from .page_planner.calendar_widget.calendar_grid import CalendarGrid
+from .page_planner.calendar_widget import CalendarWidget
+from .page_planner.calendar_grid import CalendarGrid
 
 from .page_edit_cycles.edit_cycles_layout import EditCyclesLayout
 from .page_edit_cycles.cycle_list import CyclesList
@@ -25,8 +25,8 @@ Builder.load_file(os.path.join(here, 'theme.kv'))
 Builder.load_file(os.path.join(here, 'main_layout.kv'))
 Builder.load_file(os.path.join(here, 'page_planner', 'planner_layout.kv'))
 Builder.load_file(os.path.join(here, 'page_planner', 'task_list.kv'))
-Builder.load_file(os.path.join(here, 'page_planner', 'calendar_widget', 'calendar_widget.kv'))
-Builder.load_file(os.path.join(here, 'page_planner', 'calendar_widget', 'calendar_grid.kv'))
+Builder.load_file(os.path.join(here, 'page_planner', 'calendar_widget.kv'))
+Builder.load_file(os.path.join(here, 'page_planner', 'calendar_grid.kv'))
 
 Builder.load_file(os.path.join(here, 'page_edit_cycles', 'edit_cycles_layout.kv'))
 Builder.load_file(os.path.join(here, 'page_edit_cycles', 'cycle_list.kv'))
